c7bc92c25d3c6d5e6212819c78162718ed8403f5
Dmitrii Leshchanov <leshok.dimkaa@gmail.com>
2023-01-25 14:30:10 +0200

    HW#35

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
+import { Input } from './components/Input';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Timer } from './components/Timer';
+import { JsxElement } from 'typescript';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class CityPair {
+  constructor(private f: string, private s: string) {
+
+  }
+
+  getF() {
+    return this.f;
+  }
+
+  getS() {
+    return this.s;
+  }
 }
 
+
+function App() {
+  const properties: React.CSSProperties = {
+    display: "flex",
+    flexWrap: "wrap"
+  }
+  const row: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column'
+  }
+  const [pair, setPair] = React.useState<CityPair[]>([]);
+
+  function creatingCities(value: string): string {
+    const cities: string[] = value.split('#');
+    let message = '';
+    if(cities.length % 2 != 0) {
+      message = "number of cities/countries should be even";
+      setPair([]);
+    } else {
+      setPair(getCities(cities).slice());
+    }
+    return message;
+  }
+
+  function getCities(cities: string[]): CityPair[] {
+    const pair: CityPair[] = [];
+    for(let i=0; i<cities.length; i=i+2) {
+      pair.push(new CityPair(cities[i], cities[i+1]));
+    }
+    return pair;
+  }
+
+  function getDivs(pair: CityPair[]): JSX.Element[] {
+    return pair.map(pair => {
+      return (<section style={row}>
+          <Timer cityOrCountry={pair.getF()}></Timer>
+          <Timer cityOrCountry={pair.getS()}></Timer>
+      </section>)
+    })
+  }
+
+return <section style={{display: "flex",flexDirection:"column"}}>
+   <Input placeHolder={'enter city/coutry separated by #'} inputProcess={creatingCities}/> 
+    <section style={properties}>
+   {getDivs(pair)}
+    </section>
+
+  </section>
+}
 export default App;
